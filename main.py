13f39df49117b14095fb4ebd138f5e9c77b91736
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-23 15:25:43 -0300

    cria a classificação de transação

--- main.py
+++ main.py
@@ -26,9 +26,18 @@ def processar_conversas(conteudo):
     categorias_gastos = {}
     for linha in linhas:
         match = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2} - (.*?): (.*?),\s*(.*?),\s*([+-]R\$?\d+)', linha)
-    if match:
-            nome = match.group(1).strip()
-            descricao = match.group(2).strip()
-            categoria = match.group(3).strip().capitalize()
-            valor_bruto = match.group(4).replace('R$', '').replace('.', '').strip()
-            valor = int(valor_bruto.replace('+', '').replace(',', '').replace('-', '')) * (-1 if '-' in match.group(4) else 1)
+        if match:
+                nome = match.group(1).strip()
+                descricao = match.group(2).strip()
+                categoria = match.group(3).strip().capitalize()
+                valor_bruto = match.group(4).replace('R$', '').replace('.', '').strip()
+                valor = int(valor_bruto.replace('+', '').replace(',', '').replace('-', '')) * (-1 if '-' in match.group(4) else 1)
+
+        if categoria in categorias_gastos_fixos or valor < 0:
+                    gastos += abs(valor)
+                    categorias_gastos[categoria] = categorias_gastos.get(categoria, 0) + abs(valor)
+        elif categoria in categorias_ganhos_fixos or valor > 0:
+                    ganhos += valor
+                    categorias_ganhos[categoria] = categorias_ganhos.get(categoria, 0) + valor
+    saldo = ganhos - gastos
+    return ganhos, gastos, saldo, categorias_ganhos, categorias_gastos
