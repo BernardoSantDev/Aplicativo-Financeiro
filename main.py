12a37fa703c840ebcab8b8b9830f7bb32c8ab2c6
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-23 15:22:19 -0300

    carrega chave da openai e carrega as categorias fixas

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import re
 import os                             
 from dotenv import load_dotenv        
 from openai import OpenAI             
+
+
+load_dotenv()  
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))  
+
+categorias_gastos_fixos = [
+    'Aluguel', 'Farmacia', 'Padaria', 'Mercado', 'Supermercado',
+    'Energia', 'Água', 'Luz', 'Internet', 'Transporte', 'Combustivel', 'Alimentação'
+]
+
+categorias_ganhos_fixos = ['Renda']
+
