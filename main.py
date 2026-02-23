5ff899b14e6b89682707eb6a2303970a79580975
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-23 15:20:30 -0300

    adiciona bibliotecas

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st               
+import pandas as pd                   
+import matplotlib.pyplot as plt       
+import re                             
+import os                             
+from dotenv import load_dotenv        
+from openai import OpenAI             
