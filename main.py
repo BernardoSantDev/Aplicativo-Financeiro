d249285240e5beb8688d171d339d651d2f504f03
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-02-23 15:23:37 -0300

    Cria função que lê cada linha do texto, identifica transações e classifica como ganho ou gasto

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ categorias_gastos_fixos = [
 
 categorias_ganhos_fixos = ['Renda']
 
+def processar_conversas(conteudo):
+    linhas = conteudo.split('\n')  # Divide o conteúdo por linha
+
+    ganhos = 0
+    gastos = 0
+    categorias_ganhos = {}
+    categorias_gastos = {}
+    for linha in linhas:
+        match = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2} - (.*?): (.*?),\s*(.*?),\s*([+-]R\$?\d+)', linha)
+    if match:
+            nome = match.group(1).strip()
+            descricao = match.group(2).strip()
+            categoria = match.group(3).strip().capitalize()
+            valor_bruto = match.group(4).replace('R$', '').replace('.', '').strip()
+            valor = int(valor_bruto.replace('+', '').replace(',', '').replace('-', '')) * (-1 if '-' in match.group(4) else 1)
